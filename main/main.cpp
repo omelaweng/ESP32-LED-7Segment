46559671bceef29c3bb3fdd0c22af8bea96ea345
khemmasedth <khmasade555@gmail.com>
2024-08-08 14:33:19 +0700

    3.3

--- main/main.cpp
+++ main/main.cpp
@@ -8,6 +8,10 @@ extern "C" void app_main(void)
 {
     while (1)
     {
+        s1.HardwareTest();
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        s1.DisplayBlank();
+        vTaskDelay(500 / portTICK_PERIOD_MS);
         s2.HardwareTest();
         vTaskDelay(500 / portTICK_PERIOD_MS);
         s2.DisplayBlank();
